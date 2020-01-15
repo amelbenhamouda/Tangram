4f7eac3c8419c46666b163cba4d13184153a7556
KapoueOui <aure-durand@hotmail.fr>
2020-01-15 03:59:40 -0800

    il faut changer drawbuton et faire un create buton , dans drawbutton il faut alors juste erase le button ou l'on change un elem (sur le button n° de figure pour l'instant

--- Rendu-final/include/button.hpp
+++ Rendu-final/include/button.hpp
@@ -41,7 +41,7 @@
 			 *  \param height
 			 *  \param message
 			 */
-	    	Button(T x, T y, T width, T height,  std::string message);
+	    	Button(T x, T y, T width, T height, const std::string message);
             
             /*!
 			 * 
@@ -70,13 +70,14 @@
 template <class T>
 Button<T>::Button() : x(0), y(0), width(0), height(0), message(0), interligne(0), borderColor(MLV_COLOR_BLACK), textColor(MLV_COLOR_BLACK), backgroundColor(MLV_COLOR_BLACK) {}
 template <class T>
-Button<T>::Button(T x, T y, T width, T height,  std::string message) : x(x), y(y), width(width), height(height), message(message), interligne(0), borderColor(MLV_COLOR_BLACK), textColor(MLV_COLOR_BLACK), backgroundColor(MLV_COLOR_BLACK) {}
+Button<T>::Button(T x, T y, T width, T height, const std::string message) : x(x), y(y), width(width), height(height), message(message), interligne(0), borderColor(MLV_COLOR_BLACK), textColor(MLV_COLOR_BLACK), backgroundColor(MLV_COLOR_BLACK) {}
 template <class T>
 Button<T>::~Button(){}
 
 template <typename T>
 void Button<T>::drawButton(T interligne, MLV_Color borderColor, MLV_Color textColor, MLV_Color backgroundColor) {
     // x, y	La coordonnée en X, Y de la position du coin Nord-Ouest de la boîte.
+    std::cout << "message: "<<message << std::endl;
     const char * cstr = message.c_str();
     MLV_draw_text_box(x, y, width, height, cstr, interligne, borderColor, textColor, backgroundColor, MLV_TEXT_LEFT, MLV_HORIZONTAL_CENTER, MLV_VERTICAL_CENTER);
 }
--- Rendu-final/src/board.cpp
+++ Rendu-final/src/board.cpp
@@ -18,14 +18,16 @@ void Board::drawButtonForBoard(unsigned int numFig) {
     int x = width - 300;
     int size = 25;
     std::cout << "std(numfig) " <<(std::to_string(numFig)).c_str() << std::endl;
+    std::cout << "map size: " << buttons.size() << std::endl;
     // x, y La coordonnée en X, Y de la position du coin Nord-Ouest de la boîte.
+    buttons.clear();
     buttons.insert(std::make_pair(1,Button<int>(x, y, size, heightButton, "<-")));
     buttons.insert(std::make_pair(2,Button<int>(x + heightButton + interligne, y, size, heightButton, (std::to_string(numFig)).c_str())));
     buttons.insert(std::make_pair(3,Button<int>(x + (interligne + heightButton) * 2, y, size, heightButton, "->")));
     buttons.insert(std::make_pair(4,Button<int>(x, y + heightButton + interligne, widthButton, heightButton, "Contour")));
     buttons.insert(std::make_pair(5,Button<int>(x, y + (heightButton * 2) + (interligne * 2), widthButton, heightButton, "Sauvegarder")));
     buttons.insert(std::make_pair(6,Button<int>(x, y + (heightButton * 3) + (interligne * 3), widthButton, heightButton, "Quitter")));
-
+    std::cout << "map size: " << buttons.size() << std::endl;
     for (auto button : buttons) {
         std::cout << "draw buton"<< std::endl;
         button.second.drawButton(interligne, MLV_COLOR_GREY, MLV_COLOR_BLACK, MLV_COLOR_WHITE);
